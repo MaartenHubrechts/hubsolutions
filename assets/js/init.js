94689fef8a8563d669991c9f14446b7717c6189d
Maarten <maarten.hubrechts@hotmail.com>
2024-12-15 20:56:00 +0100

    Remove jekyll-archives and update dependencies

--- assets/js/init.js
+++ assets/js/init.js
@@ -13,7 +13,7 @@ jQuery(document).ready(function () {
 
 	// tokyo_tm_color_switcher();
 	// tokyo_tm_switcher_opener();
-	tokyo_tm_cursor_switcher();
+	// tokyo_tm_cursor_switcher();
 	// tokyo_tm_menu();
 	tokyo_tm_modalbox_news();
 	tokyo_tm_popupscroll();
@@ -21,7 +21,7 @@ jQuery(document).ready(function () {
 	tokyo_tm_popupscroll_about();
 	tokyo_tm_projects();
 	tokyo_tm_portfolio();
-	tokyo_tm_cursor();
+	// tokyo_tm_cursor();
 	tokyo_tm_imgtosvg();
 	tokyo_tm_popup();
 	tokyo_tm_data_images();
